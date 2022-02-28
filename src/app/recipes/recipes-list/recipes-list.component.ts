da4895b8064c5c6e6623e417212946324853b756
emilqesja <emilqesja@outlook.com>
2022-02-28 12:18:39 +0100

    meals-project-part1

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  recipes:Recipe[] = [
+    new Recipe('A test Recipe',
+      'This is simply a test.',
+      'https://img.delicious.com.au/5aLcV7cG/del/2021/05/slow-roasted-butter-eggplant-curry-152139-2.jpg'),
+    new Recipe('A test Recipe',
+      'This is simply a test.',
+      'https://img.delicious.com.au/5aLcV7cG/del/2021/05/slow-roasted-butter-eggplant-curry-152139-2.jpg')
+  ]
 
   constructor() { }
 
